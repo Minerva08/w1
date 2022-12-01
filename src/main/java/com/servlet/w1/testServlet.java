d697ad7516c049028a93b32b472e486080876312
jaeryeongkim <elavl@naver.com>
2022-12-01 23:59:58 +0900

    @WebServlet     - name     - value     - urlPattern

--- src/main/java/com/servlet/w1/testServlet.java
+++ src/main/java/com/servlet/w1/testServlet.java
@@ -1,15 +1,18 @@
 package com.servlet.w1;
 
-import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
+@WebServlet(name = "testServlet", urlPatterns = "/practice/servlet")
+public class testServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hi~ World!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
--- src/main/webapp/test.jsp
+++ src/main/webapp/test.jsp
@@ -5,9 +5,9 @@
     <title>JSP - Hello World</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
+<h1><%= "TEST World!" %>
 </h1>
 <br/>
-<a href="hello-servlet">Hello Servlet</a>
+<a href="practice/servlet">TEST Servlet</a>
 </body>
 </html>
